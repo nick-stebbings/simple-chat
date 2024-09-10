fcf291e4b864a2fe3e01cb780efb528655a445e0
Nicholas Stebbings <nickolarse@riseup.net>
2024-09-11 07:15:27 +1200

    test to add second distinct user

--- crates/server/src/user_pool.rs
+++ crates/server/src/user_pool.rs
@@ -22,6 +22,9 @@ where
         }
     }
 
+    /**
+     * Adds a user to the user pool.
+     */
     pub async fn add_user(&self, user: User<S>) {
         if let Ok(mut hashmap) = self.users.write() {
             let username = user.username.clone();
@@ -58,5 +61,36 @@ mod tests {
 
         // Assert
         assert!(users.contains_key("anon"));
+    }
+    #[tokio::test]
+    async fn test_add_distinct_user() {
+        // Arrange
+        let (stream1, _) = duplex(64);
+        let (stream2, __) = duplex(64);
+
+        let user_pool = UserPool::<DuplexStream>::new();
+        let (tx1, rx1) = mpsc::channel(50);
+        let (tx2, rx2) = mpsc::channel(50);
+        let user1 = User {
+            username: "anon".to_string(),
+            msg_sender: tx1,
+            msg_receiver: rx1,
+            stream: stream1,
+        };
+        let user2 = User {
+            username: "anon2".to_string(),
+            msg_sender: tx2,
+            msg_receiver: rx2,
+            stream: stream2,
+        };
+
+        // Act
+        user_pool.add_user(user1).await;
+        user_pool.add_user(user2).await;
+        let users = user_pool.users.read().unwrap();
+
+        // Assert
+        assert!(users.contains_key("anon"));
+        assert!(users.contains_key("anon2"));
     }
 }
