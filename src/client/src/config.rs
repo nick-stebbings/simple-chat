7c38b10705a5c73105f7ecb17bd6a60c17f730c7
Nicholas Stebbings <nickolarse@riseup.net>
2024-09-10 17:51:03 +1200

    add clap and parse username

--- src/client/src/config.rs
+++ src/client/src/config.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 
+use clap::{arg, command, Parser};
 use serde::Deserialize;
 
 #[derive(Deserialize, Debug)]
@@ -10,4 +11,11 @@ pub struct Config {
 
 pub fn get_config() -> Result<Config, Box<dyn Error>> {
     Ok(envy::from_env::<Config>()?)
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Args {
+    #[arg(short, long, default_value = "anon")]
+    pub username: String,
 }
--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,11 +1,15 @@
 mod config;
 
+use clap::Parser;
+use config::{get_config, Args};
 use tokio::net::TcpStream;
-
-use config::get_config;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    println!("Hello, {}!", args.username);
+
     let config_values = get_config()?;
     let address = format!("{}:{}", config_values.host, config_values.port);
     let mut stream = TcpStream::connect(&address).await?;
