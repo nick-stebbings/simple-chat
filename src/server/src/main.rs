d348bbef42932e7f323b59d3d4e13a8ec89f5169
Nicholas Stebbings <nickolarse@riseup.net>
2024-09-10 17:56:21 +1200

    scaffold server entry point and modules

--- src/server/src/main.rs
+++ src/server/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+mod user;
+mod user_pool;
+
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let listener = TcpListener::bind("127.0.0.1:8080").await?;
+    println!("Server running on 127.0.0.1:8080");
+
+    Ok(())
 }
