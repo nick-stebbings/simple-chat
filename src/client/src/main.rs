6a1ab982221a02ec5105b5b6a99c373e6d88e89d
Nicholas Stebbings <nickolarse@riseup.net>
2024-09-10 17:05:43 +1200

    import config with error handling

--- src/client/src/main.rs
+++ src/client/src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod config;
+
+use config::{get_config};
+
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let config_values = get_config()?;
+    Ok(())
 }
