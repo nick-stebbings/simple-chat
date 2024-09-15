904c30841071c51fef64025763e093107a39c61b
Nicholas Stebbings <nickolarse@riseup.net>
2024-09-16 09:59:37 +1200

    refactor connection to handle commands

--- crates/common/src/connection.rs
+++ crates/common/src/connection.rs
@@ -1,43 +1,147 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
-use bytes::BytesMut;
-use futures_util::*;
-use tokio::io::{self, BufWriter};
-use tokio::net::TcpStream;
-use tokio_util::codec::{FramedRead, LinesCodec};
 
 use crate::command::Command;
-pub struct Connection {
-    stream: Option<BufWriter<TcpStream>>,
-    buffer: BytesMut,
+use futures_util::sink::SinkExt;
+use futures_util::StreamExt;
+use tokio::io::{duplex, AsyncRead, AsyncWrite, AsyncWriteExt};
+use tokio_util::codec::{Framed, LinesCodec, LinesCodecError};
+
+pub struct Connection<S> {
+    pub framed: Framed<S, LinesCodec>,
+}
+// type SerdeStream = tokio_util::codec::Framed<Framed<TcpStream, LinesCode>, Json<Command, ()>>;
+
+impl<S: AsyncWrite + AsyncRead + Unpin> Connection<S> {
+    pub fn new(stream: S) -> Self {
+        let framed = Framed::<S, LinesCodec>::new(stream, LinesCodec::new());
+        Connection { framed }
+    }
+    pub async fn send_command(&mut self, command: Command) -> Result<(), LinesCodecError> {
+        self.framed.send(command).await?;
+        <Framed<S, LinesCodec> as SinkExt<Command>>::flush(&mut self.framed).await?;
+        Ok(())
+    }
+    pub async fn read_command(&mut self) -> Result<Option<Command>, LinesCodecError> {
+        if let Some(result) = self.framed.next().await {
+            match result {
+                Ok(line) => Ok(Command::parse(&line.to_string())),
+                Err(e) => Err(e.into()),
+            }
+        } else {
+            println!("result:");
+            Ok(None)
+        }
+    }
+}
+#[tokio::test]
+async fn test_read_send_message_command() {
+    // Arrange
+    let (client, mut server) = tokio::io::duplex(64);
+    let mut connection = Connection::new(client);
+
+    // Act
+    let command_str = "send Hey!";
+    server.write_all(command_str.as_bytes()).await.unwrap();
+    server.write_all(b"\n").await.unwrap();
+    let command = connection.read_command().await.unwrap();
+
+    // Assert
+    assert_eq!(
+        command.unwrap().to_string(),
+        Command::SendMessage("Hey!".to_string()).to_string()
+    );
+}
+#[tokio::test]
+async fn test_read_leave_command() {
+    // Arrange
+    let (client, mut server) = tokio::io::duplex(64);
+    let mut connection = Connection::new(client);
+
+    // Act
+    let command_str = "leave";
+    server.write_all(command_str.as_bytes()).await.unwrap();
+    server.write_all(b"\n").await.unwrap();
+    let command = connection.read_command().await.unwrap();
+
+    // Assert
+    assert_eq!(command.unwrap().to_string(), Command::Leave.to_string());
+}
+#[tokio::test]
+async fn test_read_join_command() {
+    // Arrange
+    let (client, mut server) = tokio::io::duplex(64);
+    let mut connection = Connection::new(client);
+
+    // Act
+    let command_str = "join Davey";
+    server.write_all(command_str.as_bytes()).await.unwrap();
+    server.write_all(b"\n").await.unwrap();
+    let command = connection.read_command().await.unwrap();
+
+    // Assert
+    assert_eq!(
+        command.unwrap().to_string(),
+        Command::Join("Davey".to_string()).to_string()
+    );
 }
 
-impl Connection {
-    pub async fn new(
-        address: &str,
-    ) -> Result<Connection, Box<dyn std::error::Error + Send + Sync>> {
-        match TcpStream::connect(address).await {
-            Ok(socket) => Ok(Connection {
-                stream: Some(BufWriter::new(socket)),
-                buffer: BytesMut::with_capacity(4 * 1024),
-            }),
-            Err(e) => {
-                eprintln!("Failed to connect to the server at {}: {}", address, e);
-                Err(Box::new(e))
-            }
-        }
-    }
+#[tokio::test]
+async fn test_send_send_message_command() {
+    // Arrange
+    let (client, mut server) = duplex(64);
+    let mut connection = Connection::new(client);
 
-    pub async fn read_command(&mut self) -> io::Result<Option<Command>> {
-        if let Some(ref mut stream) = self.stream {
-            let mut framed = FramedRead::new(stream, LinesCodec::new());
+    // Act
+    let command = Command::SendMessage("Hey!".to_string());
+    connection.send_command(command).await.unwrap();
 
-            while let Ok(Some(line)) = framed.try_next().await {
-                if let Some(command) = Command::parse(&line) {
-                    return Ok(Some(command));
-                }
-            }
-        }
-        Ok(None)
-    }
+    let mut framed = Framed::new(&mut server, LinesCodec::new());
+    let line = framed.next().await.unwrap().unwrap();
+
+    // Assert
+    assert_eq!(
+        Command::parse(&line.to_string()).unwrap().to_string(),
+        "Hey!"
+    );
+}
+
+#[tokio::test]
+async fn test_send_and_read_subsequent_messages() {
+    // Arrange
+    let (client, server) = tokio::io::duplex(64);
+    let mut connection_client = Connection::new(client); // Use the client side for sending
+    let mut connection_server = Connection::new(server); // Use the server side for reading
+
+    // Act
+    let command1 = Command::SendMessage("Hey".to_string());
+    connection_client.send_command(command1).await.unwrap();
+
+    let command2 = Command::SendMessage("Jude!".to_string());
+    connection_client.send_command(command2).await.unwrap();
+
+    let command3 = Command::SendMessage("Don't let me down".to_string());
+    connection_client.send_command(command3).await.unwrap();
+
+    // Read commands
+    let read_command1 = connection_server
+        .read_command()
+        .await
+        .expect("No more commands to read")
+        .expect("Not a valid command");
+    let read_command2 = connection_server
+        .read_command()
+        .await
+        .expect("No more commands to read")
+        .expect("Not a valid command");
+    let read_command3 = connection_server
+        .read_command()
+        .await
+        .expect("No more commands to read")
+        .expect("Not a valid command");
+
+    // Assert
+    assert_eq!(read_command1.to_string(), "Hey");
+    assert_eq!(read_command2.to_string(), "Jude!");
+    assert_eq!(read_command3.to_string(), "Don't let me down");
 }
--- crates/server/src/user.rs
+++ crates/server/src/user.rs
@@ -1,9 +1,13 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
+use common::command::Command;
+use common::connection::Connection;
 use std::sync::Arc;
 use tokio::io::{AsyncRead, AsyncWrite};
 use tokio::sync::{mpsc, Mutex};
+
+use crate::user_pool::UserPool;
 
 pub struct User<S>
 where
@@ -12,7 +16,28 @@ where
     pub username: String,
     pub msg_sender: mpsc::Sender<String>,
     pub msg_receiver: Receiver<String>,
-    pub stream: S,
+    pub conn: Connection<S>,
 }
 
 type Receiver<S> = Arc<Mutex<mpsc::Receiver<S>>>;
+
+impl<S: AsyncRead + AsyncWrite + Unpin> User<S> {
+    pub async fn handle_commands(&mut self, user_pool: Arc<&UserPool<S>>) {
+        loop {
+            match self.conn.read_command().await.unwrap() {
+                Some(Command::SendMessage(message)) => {
+                    println!("user handling command.. {}", message.clone());
+                    let _send = self.msg_sender.send(format!("send {}", message)).await;
+                }
+                Some(Command::Leave) => {
+                    println!("user handling leave command.. ");
+                    let _send = self.msg_sender.send("leave".to_string()).await;
+                    break;
+                }
+                _ => {
+                    break;
+                }
+            }
+        }
+    }
+}
